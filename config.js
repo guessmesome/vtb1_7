31f2bff2924afdb78ac43bc3fdc6dd3099cad8fe
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-12 12:24:38 +0700

    upd

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    products: {
+        1: 'https://vldmnt.ru/go/s2swbbyrq5',
+        2: 'https://trnsfx.ru/go/sqlj9mg8tc',
+        3: 'https://trnsfx.ru/go/s7hm7flrrr',
+        4: 'https://trnsfx.ru/go/swejgb27x2'
+    }
+};
